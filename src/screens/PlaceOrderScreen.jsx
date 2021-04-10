a67d727b3626d55aeee38d685cc810c325dbd072
Shashank <s_shashank@outlook.com>
2021-04-10 21:15:41 +0530

    added place order screen

---
+++ src/screens/PlaceOrderScreen.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
+import CheckoutSteps from "../components/CheckoutSteps";
+import Message from "../components/Message";
+import { Link } from "react-router-dom";
+
+const PlaceOrderScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  cart.itemsPrice = cart.cartItems
+    .reduce((acc, item) => acc + item.price * item.qty, 0)
+    .toFixed(2);
+
+  cart.shippingPrice = (cart.itemsPrice > 100 ? 0 : 10).toFixed(2);
+
+  cart.taxPrice = Number(0.082 * cart.itemsPrice).toFixed(2);
+
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2);
+
+  return (
+    <>
+      <Row className="my-5 justify-content-center">
+        <Col md={6}>
+          <CheckoutSteps step1 step2 step3 step4 />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={8}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>SHIPPING</h3>
+              <p>
+                <strong>Shipping Address: </strong>
+                {shippingAddress.address}, {shippingAddress.city},{" "}
+                {shippingAddress.state}-{shippingAddress.pincode},{" "}
+                {shippingAddress.country}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>PAYMENT METHOD</h3>
+              <p>
+                <strong>Payment Method: </strong>
+                {cart.paymentMethod}
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3>ORDER ITEMS</h3>
+              {cart.cartItems.length === 0 ? (
+                <Message variant="info">You cart is empty.</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={2}>
+                          <Image src={item.image} alt={item.name} fluid />
+                        </Col>
+                        <Col md={6}>
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={2}>${item.price}</Col>
+                        <Col md={2}>{item.qty}</Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2 className="text-center">ORDER SUMMARY</h2>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Item:</Col>
+                  <Col>${cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping:</Col>
+                  <Col>${cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax:</Col>
+                  <Col>${cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total:</Col>
+                  <Col>${cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className="btn-block">Place Order</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default PlaceOrderScreen;
